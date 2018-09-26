27f319ed0d0512f22e36602e944af08b0ed817d2
geraldine <geraldine.llanos.bazan@gmail.com>
2018-09-26 03:44:52 -0500

    toronja

---
+++ src/VIEW/Geraldine.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VIEW;
+
+/**
+ *
+ * @author user
+ */
+public class Geraldine {
+    public String nombre = "Geraldine";
+    private int edad = 22;
+}
